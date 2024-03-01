1784892bcec9faebb74f904df76dfe7aa54f3209
Constantine999 <desperado904@gmail.com>
2024-03-01 16:58:49 +0300

    Update query

--- query.py
+++ query.py
@@ -1,23 +1,29 @@
+import asyncio
+
+from sqlalchemy import func, select
 from sqlalchemy.engine import Result
 
-from schemas import Info, ResponseInfo
 from database import new_session
+from schemas import Info, ResponseInfo
 from src.models import Post
 
-from sqlalchemy import select, func
+lock = asyncio.Lock()
 
 
 async def add_client(data: Info) -> list[ResponseInfo]:
-    async with new_session() as session:
-        total = await session.execute(select(func.count()).select_from(Post).where(Post.name == data.name))
-        count_post = total.scalar()
-        client = Post(**data.model_dump(), messages_count=count_post + 1)
-        session.add(client)
-        await session.commit()
-        stmt = select(Post).order_by(-Post.sequence_number).limit(10)
-        result: Result = await session.execute(stmt)
+    async with lock:
+        await asyncio.sleep(0.01)
+        async with new_session() as session:
+            total = await session.execute(select(func.count()).select_from(Post).where(Post.name == data.name))
+            await asyncio.sleep(0)
+            count_post = total.scalar()
+            client = Post(**data.model_dump(), messages_count=count_post + 1)
+            session.add(client)
+            await session.commit()
 
-        return [
-            ResponseInfo.model_validate(post, from_attributes=True)
-            for post in result.scalars().all()
-        ]
+            stmt = select(Post).order_by(-Post.sequence_number).limit(10)
+            result: Result = await session.execute(stmt)
+            return [
+                ResponseInfo.model_validate(post, from_attributes=True)
+                for post in result.scalars().all()
+            ]
