dfe42dbe37c795ec518918fa763988b3e4622bc9
konstantin <desperado904@gmail.com>
2024-03-03 03:47:57 +0300

    refactor code

--- query.py
+++ query.py
@@ -12,17 +12,12 @@ lock = asyncio.Lock()
 
 async def add_client(data: Info) -> list[ResponseInfo]:
     async with lock:
-        await asyncio.sleep(0.01)
         async with new_session() as session:
-            total = await session.execute(select(func.count()).select_from(Post).where(Post.name == data.name))
-            await asyncio.sleep(0)
-            count_post = total.scalar()
-            client = Post(**data.model_dump(), messages_count=count_post + 1)
-            session.add(client)
+            session.add(Post(**data.model_dump()))
             await session.commit()
-
             stmt = select(Post).order_by(-Post.sequence_number).limit(10)
             result: Result = await session.execute(stmt)
+
             return [
                 ResponseInfo.model_validate(post, from_attributes=True)
                 for post in result.scalars().all()
