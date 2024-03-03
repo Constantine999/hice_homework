226e8182e30257fec0b17b1678156de73b7b3f23
konstantin <desperado904@gmail.com>
2024-03-03 03:47:28 +0300

    small changes

--- src/client.py
+++ src/client.py
@@ -7,20 +7,21 @@ import aiohttp
 
 def get_senders() -> list[dict[str, str]]:
     """Генерирует список из 100 словарей с рандомными значениями"""
-    SENDERS: tuple[str] = \
+    senders: tuple[str, ...] = \
         ("Джеймс Гослинг", "Деннис Ритчи", "Бьёрн Страуструп", "Гвидо ван Россум", "Брендан Эйх",
-         "Ларри Уолл", "Расмус Лердорф", "Юкихиро Мацумото", "Джон Маккарти", "Никлаус Вирт")
+         "Ларри Уолл", "Расмус Лердорф", "Юкихиро Мацумото", "Джон Маккарти", "Никлаус Вирт"
+         )
 
     with open("./text.txt", encoding="utf-8") as text:
         result: list[str] = list(map(str.strip, text.readlines()))
 
-    return [dict(name=choice(SENDERS), text=choice(result)) for _ in range(100)]
+    return [dict(name=choice(senders), text=choice(result)) for _ in range(100)]
 
 
 async def send_request(data_to_send: dict[str, str]) -> None:
     """Корутина для отправления запроса на сервер"""
     async with aiohttp.ClientSession() as session:
-        port: int = choice((8000, 9000, 10000))
+        port: int = choice((9000, 10000))
         await session.post(
             url=f"http://127.0.0.1:{port}/api/v1/",
             json=data_to_send
@@ -38,8 +39,9 @@ async def main() -> None:
     await asyncio.gather(*[generator_group_coroutines() for _ in range(50)])
     duration = perf_counter() - start
 
-    print(f"Общая продолжительность всех запросов = {round(duration, 3)} секунд")
-    print(f"Среднее время выполнения одного запроса = {round(duration / 5000, 3)} секунд")
+    print(f"Общая продолжительность всех запросов = {round(duration, 3)} секунд.")
+    print(f"Среднее время выполнения одного запроса = {round(duration / 5000, 3)} секунд.")
 
 
-asyncio.run(main())
+for _ in range(10):
+    asyncio.run(main())
